1dec28f41d537c8b810dc406253dba3b66af20fa
enescakr55 <enescakr01@hotmail.com>
2025-07-22 23:54:48 +0300

    Add project files.

---
+++ ApiService/ComfyUiEndpoints.cs
@@ -0,0 +1,18 @@
+﻿namespace ApiService
+{
+  public static class ComfyUiEndpoints
+  {
+    public static WebApplication BuildEndpoints(this WebApplication app)
+    {
+      app.MapGet("api/v1/comfy-ui/schemes/list", (HttpContext httpContext) =>
+      {
+        
+      });
+      app.MapPost("/api/v1/comfy-ui/schemes/{scheme}/images/generate", (HttpContext httpContext,string scheme) =>
+      {
+        return Results.Ok(scheme);
+      });
+      return app;
+    }
+  }
+}
